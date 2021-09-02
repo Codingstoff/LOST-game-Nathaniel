7122641c69f220b963478afe889e7d3fe331bcbc
Codingstoff <73205292+Codingstoff@users.noreply.github.com>
2021-09-02 17:08:29 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ var  platform1Img,platform2Img,platform3Img,wall1Img;
 var platform1,platform2,platform3,platform4,platform5,platform6,platform7,platform8,platform9,platform10;
 var collectible,collectible2,coinImg
 var CoinGroup;
-var gameState = 1;
+var gameState = 0;
 var score = 0;
 var bRoomChange = false;
 var jumpSound,coinSound;
